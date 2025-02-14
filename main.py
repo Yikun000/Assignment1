7bb85f83fd8027c76e4b6916e7c955f06f757f9c
YikunChen <yikun.chen@student.tdsb.on.ca>
2025-02-14 16:22:22 -0500

    shoveled

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import random
 print("hello world")
+rock =1
+paper =2
+sciccor =3
+print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
+      + "Rock vs Paper -> Paper wins \n"
+      + "Rock vs Scissors -> Rock wins \n"
+      + "Paper vs Scissors -> Scissors wins \n")
