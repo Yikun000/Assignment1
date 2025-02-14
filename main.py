9b94a140852e786ea0c8dc72053b48336b7ac01e
YikunChen <yikun.chen@student.tdsb.on.ca>
2025-02-14 16:32:30 -0500

    snow

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
-print("hello world")
-rock =1
-paper =2
-sciccor =3
 print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"
       + "Paper vs Scissors -> Scissors wins \n")
+while True:
+
+    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+
+    
+    choice = int(input("Enter your choice: "))
